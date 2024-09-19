c3f97b2aac9b89e13db01c6e127396533a4943c2
Swarnava-paul <swarnavapaul108@gmail.com>
2024-09-19 18:48:13 +0530

    m

--- Backend/Config/Connection.db.js
+++ Backend/Config/Connection.db.js
@@ -1,8 +1,6 @@
 const {initializeApp,applicationDefault,cert} = require('firebase-admin/app')
 const {getFirestore, Timestamp, FieldValue, Filter} = require('firebase-admin/firestore');
-
-const serviceAccount = require('./firebase.json');
-
+const serviceAccount = require('./firebase.json')
 
 initializeApp({
     credential: cert(serviceAccount)
